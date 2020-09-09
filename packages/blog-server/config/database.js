48c4d5167e489cb040aed4e2a7e735b7b6f0ebd3
Steve Shea <stevenlsjr@gmail.com>
2020-09-09 15:21:00 -0400

    added server

--- packages/blog-server/config/database.js
+++ packages/blog-server/config/database.js
@@ -4,12 +4,18 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'postgres',
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'apiuser'),
+        password: env('DATABASE_PASSWORD', 'apiuser'),
+        schema: 'public',
       },
       options: {
         useNullAsDefault: true,
       },
+
     },
   },
 });
