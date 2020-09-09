625e380d784069a33b05236e84e706d68283559e
Steve Shea <stevenlsjr@gmail.com>
2020-09-09 15:08:17 -0400

    first commit

---
+++ packages/blog-server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8b7184978fbf7ea8c35412ed4733e549'),
+    },
+  },
+});
